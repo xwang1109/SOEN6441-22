a0914ee78a832154d3a877433b3747284103519b
Parisa <siparisa@gmail.com>
2018-10-17 22:07:46 -0400

    LoadMapTestFromDialog

--- src/test/models/MapTests.java
+++ src/test/models/MapTests.java
@@ -2,32 +2,44 @@ package test.models;
 import static org.junit.Assert.*;
 
 import models.map.GameState;
+import models.map.Map;
+import views.game.ViewState;
+
 import org.junit.Before;
 import org.junit.Test;
 import java.util.*;
+
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileSystemView;
+
 import java.io.*;
 
 
 
 
 public class MapTests  {
-	File fileTest;
+	File selectedFile;
 	@Before
 	public void InitiateMap() {
-		 fileTest=new File("C:\\Users\\MAIN\\Documents\\Iran.map");
-		//File selectedFileT = GameState.getInstance().getSelectedFile();
-		//boolean resultT = GameState.getInstance().loadMapFromFile(fileTest);
+
+			JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+
+			int returnValue = jfc.showOpenDialog(null);
+			if (returnValue == JFileChooser.APPROVE_OPTION) {
+				File selectedFile = jfc.getSelectedFile();
+				GameState.getInstance().setSelectedFile(selectedFile);
+			}			
+
  
 				
 	}
    @Test
    public void TestMap() {
-	   boolean resultT = GameState.getInstance().loadMapFromFile(fileTest);
-	   //boolean result2;
-	   //MapTests maptest=new MapTests();
-	   //result2=maptest.InitiateMap()
+
+		File selectedFile = GameState.getInstance().getSelectedFile();
+		boolean result = GameState.getInstance().loadMapFromFile(selectedFile);
 	   
-	   assertTrue(resultT);
+	   assertTrue(result);
    }
  
 }
