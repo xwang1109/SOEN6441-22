2846df90e8b2c84720215e9369810c4fdbcecc65
Parisa <siparisa@gmail.com>
2018-10-17 21:29:57 -0400

    TestMAp2

--- src/test/models/MapTests.java
+++ src/test/models/MapTests.java
@@ -1,5 +1,8 @@
 package test.models;
 import org.junit.Assert.*;
+
+import models.map.GameState;
+
 import org.junit.Before;
 import org.junit.Test;
 import java.util.*;
@@ -10,12 +13,16 @@ import java.io.*;
 
 public class MapTests {
 	@Before
-	public void InitiateMap(File mapFile) {
-		FileReader readerTest=new FileReader(mapFile);
-		BufferedReader bufferedReader=new BufferedReader(readerTest);
-		 
-		
-		
+	public void InitiateMap() {
+		File fileTest=new File("C:\\Users\\MAIN\\Documents\\Iran.map");
+		File selectedFileT = GameState.getInstance().getSelectedFile();
+		boolean resultT = GameState.getInstance().loadMapFromFile(fileTest);
+ 
+				
 	}
-
+   @Test
+   public void TestMap() {
+	   assretTrue(resultT);
+   }
+   
 }
