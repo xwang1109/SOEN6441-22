3e778ef1354f9f7fc8d98ec7aebc9e4de6d20834
Lin <36003947+lilin7@users.noreply.github.com>
2018-10-18 02:02:59 -0400

    refined player set up view, added javadoc

--- src/views/game/PlayerSetupView.java
+++ src/views/game/PlayerSetupView.java
@@ -12,34 +12,41 @@ import models.map.GameState;
 import views.map.MapCountryPanel;
 
 import javax.swing.JSplitPane;
+import javax.swing.JTextArea;
 import javax.swing.JDesktopPane;
 import javax.swing.JFrame;
-
+/**
+ * Class PlayerSetupView is the view for player to start up by selecting number of players
+ * @author 
+ * @see GameState.getInstance().getMap(), 
+ */
 public class PlayerSetupView {
 
 	public PlayerSetupView(JPanel controlPanel,ViewState frame) {
-		// feed the beast
 		
 		FlowLayout fl_controlPanel = (FlowLayout) controlPanel.getLayout();
 		fl_controlPanel.setAlignment(FlowLayout.LEADING);
 		
+		//Prompt user to select number of players
+		JTextArea selectNoOfPlayers = new JTextArea("Select Number of Players:");
+				
+		controlPanel.add(selectNoOfPlayers);
+		
+		//set the drop-down box for selecting number of players, and set the options to be integer between 2 and 5
 		JComboBox numberOfPlayer = new JComboBox();
 		for (int i = 2; i < 6; i++) {
 			numberOfPlayer.addItem(i);
 		}
 		
-		JButton newGameButton = new JButton("STARTTHEGAME");
+		JButton newGameButton = new JButton("START THE GAME");
+		
+		//receive user clicking ""START THE GAME" by GameStartController
 		newGameButton.addActionListener(new GameStartController(numberOfPlayer));
 		controlPanel.add(numberOfPlayer);
 		controlPanel.add(newGameButton);
 		
 		MapCountryPanel mapPanel = frame.getMapPanel();
 		
-		mapPanel.addCountryTableForMap(GameState.getInstance().getMap());
-		//JPanel mapPane = new JPanel();
-		//mapPane.add(mapPanel);
-		
-		
-		
+		mapPanel.addCountryTableForMap(GameState.getInstance().getMap());	
 	}
 }
