44ca3e400b6175b013ebf1247383dcfcd31643b0
Parisa <siparisa@gmail.com>
2018-10-17 23:07:37 -0400

    MapTestCase

--- src/test/models/MapTests.java
+++ src/test/models/MapTests.java
@@ -9,8 +9,8 @@ import org.junit.Before;
 import org.junit.Test;
 import java.util.*;
 
-import javax.swing.JFileChooser;
-import javax.swing.filechooser.FileSystemView;
+//import javax.swing.JFileChooser;
+//import javax.swing.filechooser.FileSystemView;
 
 import java.io.*;
 
@@ -19,27 +19,40 @@ import java.io.*;
 
 public class MapTests  {
 	File selectedFile;
+	File fileCorrectMap;
+	File fileFalseConnection;
+	File fileFalseContinent;
+
+	
 	@Before
 	public void InitiateMap() {
 
-			JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+			//JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
 
-			int returnValue = jfc.showOpenDialog(null);
-			if (returnValue == JFileChooser.APPROVE_OPTION) {
-				File selectedFile = jfc.getSelectedFile();
-				GameState.getInstance().setSelectedFile(selectedFile);
+			//int returnValue = jfc.showOpenDialog(null);
+			//if (returnValue == JFileChooser.APPROVE_OPTION) {
+			//	File selectedFile = jfc.getSelectedFile();
+				//GameState.getInstance().setSelectedFile(selectedFile);
+		fileCorrectMap=new File("C:\\Users\\MAIN\\Documents\\GitHub\\SOEN6441-22\\TestCaseMapFiles\\AlabamaCorrect.map");
+		fileFalseConnection=new File("C:\\Users\\MAIN\\Documents\\GitHub\\SOEN6441-22\\TestCaseMapFiles\\AlabamaCorrect.map\\IranFalseConnection.MAP");
+		fileFalseContinent=new File("C:\\Users\\MAIN\\Documents\\GitHub\\SOEN6441-22\\TestCaseMapFiles\\AlabamaFalseContinent.map");
+
 			}			
 
  
 				
-	}
+
    @Test
    public void TestMap() {
 
-		File selectedFile = GameState.getInstance().getSelectedFile();
-		boolean result = GameState.getInstance().loadMapFromFile(selectedFile);
-	   
-	   assertTrue(result);
+
+		boolean resultCorrectMap = GameState.getInstance().loadMapFromFile(fileCorrectMap);
+		boolean resultFalseConnection = GameState.getInstance().loadMapFromFile(fileFalseConnection);
+		boolean resultFalseContinent = GameState.getInstance().loadMapFromFile(fileFalseContinent);
+		
+	   assertTrue(resultCorrectMap);
+	   assertFalse(resultFalseConnection);
+	   assertFalse(resultFalseContinent);
    }
  
 }
