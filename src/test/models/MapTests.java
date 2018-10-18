8cb43c8962e8e80638b3a4d7c565bfba40647787
Parisa <siparisa@gmail.com>
2018-10-17 21:31:38 -0400

    t3

--- src/test/models/MapTests.java
+++ src/test/models/MapTests.java
@@ -2,7 +2,6 @@ package test.models;
 import org.junit.Assert.*;
 
 import models.map.GameState;
-
 import org.junit.Before;
 import org.junit.Test;
 import java.util.*;
@@ -11,18 +10,23 @@ import java.io.*;
 
 
 
-public class MapTests {
+public class MapTests extends TestCase {
 	@Before
 	public void InitiateMap() {
 		File fileTest=new File("C:\\Users\\MAIN\\Documents\\Iran.map");
-		File selectedFileT = GameState.getInstance().getSelectedFile();
-		boolean resultT = GameState.getInstance().loadMapFromFile(fileTest);
+		//File selectedFileT = GameState.getInstance().getSelectedFile();
+		//boolean resultT = GameState.getInstance().loadMapFromFile(fileTest);
  
 				
 	}
    @Test
    public void TestMap() {
+	   boolean resultT = GameState.getInstance().loadMapFromFile(fileTest);
+	   //boolean result2;
+	   //MapTests maptest=new MapTests();
+	   //result2=maptest.InitiateMap()
+	   
 	   assretTrue(resultT);
    }
-   
+ 
 }
