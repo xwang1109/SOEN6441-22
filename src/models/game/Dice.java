f9ea512fe2f143d507761cf4544391ea2758dff7
Parisa <siparisa@gmail.com>
2018-11-06 16:39:34 -0500

    Update src/models/game/Dice.java

--- src/models/game/Dice.java
+++ src/models/game/Dice.java
@@ -1,15 +1,42 @@
 package models.game;
+import java.util.Random;
 
 public class Dice {
 	private int numberOfDots;
+	
+	
+
 	
 	public int getNumberOfDots() {
 		return numberOfDots;
 	}
 
 	public Dice() {
-		// to do roll dice
+		
 	}
 	
-	
+	 public int[] attackerDiceRandomFill(int numberAttackerDice){
+
+		 	int[] attackerDice = new int[numberAttackerDice];
+		    Random randAttcker = new Random();
+		    
+		    for(int i=0;i<attackerDice.length;i++)
+		    {
+		    	attackerDice[i] = randAttcker.nextInt();
+		    	
+		    }
+		    return attackerDice;
+		    }
+	 public int[] defenderDiceRandomFill(int numberDefenderDice){
+		 	
+		 int[] defenderDice = new int[numberDefenderDice];
+		    Random randDefender = new Random();
+		    
+		    for(int i=0;i<defenderDice.length;i++)
+		    {
+		    	defenderDice[i] = randDefender.nextInt();
+		    	
+		    }
+		    return defenderDice;
+		    }
 }
