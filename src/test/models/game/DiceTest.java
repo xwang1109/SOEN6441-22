a1c98f27da0438d4d4fbec43669787d31a122507
Parisa <siparisa@gmail.com>
2018-11-08 16:08:46 -0500

    Update src/test/models/game/DiceTest.java

--- src/test/models/game/DiceTest.java
+++ src/test/models/game/DiceTest.java
@@ -1,14 +1,15 @@
+
+package test.models.game;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import models.game.Dice;
+
 /**
  * this is the test Case for checking dice value between 6 & 1
  * @author Parisa
  * @version 2.0.0
  */
-package test.models.game;
-import static org.junit.Assert.*;
-import org.junit.Before;
-import org.junit.Test;
-
-import models.game.Dice;
 
 public class DiceTest {
 int[] diceRoll;
