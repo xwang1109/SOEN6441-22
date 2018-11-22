6d0db6c57aea36427a7ae0026b8177923901c71b
BellaBing <belayu224@gmail.com>
2018-11-22 22:27:50 +0800

    more temporarily

--- src/models/game/Player.java
+++ src/models/game/Player.java
@@ -441,12 +441,12 @@ public class Player {
 		else return false;
 	}
 
-	public Strategy getStrategy() {
-		return strategy;
-	}
-
-	public void setStrategy(Strategy strategy) {
-		this.strategy = strategy;
-	}	
+//	public Strategy getStrategy() {
+//		return strategy;
+//	}
+//
+//	public void setStrategy(Strategy strategy) {
+//		this.strategy = strategy;
+//	}	
 	
 }
