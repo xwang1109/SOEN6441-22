482561b98f9b04b40ec71886e8826b91238e18ad
BellaBing <belayu224@gmail.com>
2018-11-27 02:14:41 +0800

    add more logging to figure our what's wrong with attack

--- src/models/game/GameState.java
+++ src/models/game/GameState.java
@@ -336,7 +336,11 @@ public class GameState extends Observable {
 	 * End turn for current player, and set the "currentPlayer" to next player
 	 */
 	public void endPlayerTurn() {
-	
+		
+		Player p=GameState.getInstance().getCurrentPlayer();
+		System.out.println("Player "+currentPlayer+" finishes turn "+turns+" with "+
+		p.getArmyNumber()+" army and "+p.getCountryList().size()+" country"
+		);
 		currentPlayer = ++currentPlayer % playerList.size();
 		turns++;
 		setChanged();
