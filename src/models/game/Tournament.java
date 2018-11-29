1b9561c413892abd03ee349154b1e7c663f48330
Lin <36003947+lilin7@users.noreply.github.com>
2018-11-29 16:02:59 -0500

    Game model: refined javadoc and coding standard

--- src/models/game/Tournament.java
+++ src/models/game/Tournament.java
@@ -8,8 +8,6 @@ import models.game.GameState.Phase;
 import models.map.Map;
 import views.game.StateView;
 public class Tournament {
-
-	
 	List<Map> maps;
 	List<Player> players;
 	int numOfGames;
@@ -17,38 +15,92 @@ public class Tournament {
 	
 	String [][] results;
 	
-	
+	/**
+	 * get map
+	 * @return List<Map>
+	 */
 	public List<Map> getMaps() {
 		return maps;
 	}
+	/**
+	 * set map
+	 * @param maps
+	 */
 	public void setMaps(List<Map> maps) {
 		this.maps = maps;
 	}
+	
+	/**
+	 * get the result
+	 * @return String[][]
+	 */
 	public String[][] getResults() {
 		return results;
 	}
+	
+	/**
+	 * set the result
+	 * @param results
+	 */
 	public void setResults(String[][] results) {
 		this.results = results;
 	}
+	
+	/**
+	 * get the player list
+	 * @return List<Player>
+	 */
 	public List<Player> getPlayers() {
 		return players;
 	}
+	
+	/**
+	 * set the player list
+	 * @param players
+	 */
 	public void setPlayers(List<Player> players) {
 		this.players = players;
 	}
+	
+	/**
+	 * get the number of games
+	 * @return int number of games
+	 */
 	public int getNumOfGames() {
 		return numOfGames;
 	}
+	
+	/**
+	 * set the number of games
+	 * @param numOfGames
+	 */
 	public void setNumOfGames(int numOfGames) {
 		this.numOfGames = numOfGames;
 	}
+	
+	/**
+	 * get turns
+	 * @return int
+	 */
 	public int getTurns() {
 		return turns;
 	}
+	
+	/**
+	 * set turns
+	 * @param turns
+	 */
 	public void setTurns(int turns) {
 		this.turns = turns;
 	}
 	
+	/**
+	 * constructor of class Tournament
+	 * @param maps
+	 * @param players
+	 * @param numOfGames
+	 * @param turns
+	 */
 	public Tournament(List<Map> maps, List<Player> players, int numOfGames, int turns) {
 		super();
 		this.maps = maps;
@@ -58,13 +110,15 @@ public class Tournament {
 		results=new String[maps.size()][numOfGames];
 	}
 	
+	/**
+	 * to run the tournament
+	 */
 	public void run()
 	{
 		for(int m=0;m<maps.size();m++)
 		{			
 			Map map=maps.get(m);
-			for(int i=0;i<this.numOfGames;i++)
-			{
+			for(int i=0;i<this.numOfGames;i++){
 				GameState.getInstance().setPlayerList(players);  
 				GameState.getInstance().setMap(map);
 				
@@ -76,13 +130,11 @@ public class Tournament {
 					
 					p.doStrategySetup();
 					GameState.getInstance().endPlayerTurn();
-
 				}
 				
 				boolean finished=false;
 				
-				for(int currentTurn=0;currentTurn<turns;currentTurn++)
-				{
+				for(int currentTurn=0;currentTurn<turns;currentTurn++){
 					for(int j=0;j<players.size();j++){
 						Player p=players.get(j);
 						GameState.getInstance().setPhase(Phase.REINFORCEMENT);
@@ -107,7 +159,6 @@ public class Tournament {
 					}
 				}
 				
-				
 				if(!finished){
 					results[m][i]="Draw";
 				}
@@ -115,9 +166,12 @@ public class Tournament {
 		}
 		report();
 	}
-	private void report()
-	{
-		   StringBuilder sb = new StringBuilder();
+	
+	/**
+	 * generate the report the the tournament
+	 */
+	private void report() {
+		   	StringBuilder sb = new StringBuilder();
 	        sb.append("<html><body><table border=1>");
 
 	        sb.append("<tr>");
@@ -138,21 +192,16 @@ public class Tournament {
 	                sb.append("Map "+(m+1));
 	                sb.append("</td>");
 	                
-	                
 	            for (int j=0;j<numOfGames;j++) {
 	                sb.append("<td>");
 	                sb.append(results[m][j]);
 	                sb.append("</td>");
-
 	            }
 	            sb.append("</tr>");
 	        }
 	        sb.append("</table>");
 	        JLabel html = new JLabel(sb.toString());
 
-	        JOptionPane.showMessageDialog(null, html);
-	        
+	        JOptionPane.showMessageDialog(null, html);       
 	}
-	
-	
 }
